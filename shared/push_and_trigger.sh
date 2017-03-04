c8666d4b9bfd27b0fdc14bfa5d03de6386f2d7cf
Jon Jagger <jon@jaggersoft.com>
2017-03-04 12:36:35 +0000

    copied files from ruby repo that are used in many .travis.yml scripts

---
+++ shared/push_and_trigger.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+
+# Script used in .travis.yml files to [docker push] a
+# successfully built docker image to dockerhub followed,
+# if pushed successfully, by triggering the .travis.yml
+# files in dependent git repos.
+
+# I do NOT run this in .travis.yml's after_success: section
+# because when commands in the after_success: section fail
+# they do NOT fail the travis build.
+# https://github.com/travis-ci/travis-ci/issues/758
+
+# This file is itself curl'd by several cyber-dojo repos'
+# .travis.yml files which is why it curl's trigger-build.js
+# The .travis.yml file running this needs to set
+# language: node_js
+
+# Needs DOCKER_USERNAME and DOCKER_PASSWORD Travis
+# environment variables set for the dockerhub repo the
+# docker image is being pushed to.
+
+if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
+  BRANCH=${TRAVIS_BRANCH}
+else
+  BRANCH=${TRAVIS_PULL_REQUEST_BRANCH}
+fi
+
+if [ "${BRANCH}" == "master" ]; then
+  docker login --username "${DOCKER_USERNAME}" --password "${DOCKER_PASSWORD}"
+  TAG_NAME=$(basename ${TRAVIS_REPO_SLUG})
+  docker push cyberdojo/${TAG_NAME}
+  echo "PUSHED cyberdojo/${TAG_NAME} to dockerhub"
+  npm install travis-ci
+  script=trigger-build.js
+  curl -O https://raw.githubusercontent.com/cyber-dojo/ruby/master/${script}
+  node ${script} ${*}
+fi
---
+++ shared/trigger-build.js
@@ -0,0 +1,66 @@
+
+// This script is curl'd in several .travis.yml files to
+// trigger the .travis.yml files in dependent git repos
+// (after [docker push]'ing a docker image to dockerhub).
+
+// Dependent git repos are named in the argv command line.
+// eg $ node trigger-build.js cyber-dojo/web
+
+// I do NOT run this in .travis.yml's after_success: section
+// because when commands in the after_success: section fail
+// they do NOT fail the travis build.
+// https://github.com/travis-ci/travis-ci/issues/758
+
+// Relies on Travis environment variable GITHUB_TOKEN being set...
+//
+// 1. Create the personal access token on github
+//    o) Click your icon on the top-right and click [Settings] entry
+//    o) Click [Personal access tokens] link on the left
+//    o) Click [Generate new token] button
+//    o) Select your permissions (for Travis, all you need is public_repo)
+//    o) Once created, copy the token to your clipboard
+//
+// 2. Configure Travis to expose this token securely to this script.
+//    o) Click [More options] on the top right and select [Settings]
+//    o) Enter a new environment variable Name = GITHUB_TOKEN
+//    o) In the Value field, paste in your token from step 1
+//    o) Ensure [Display value in build log] is showing OFF
+//    o) Click [Add] on the right
+//
+// This file is curl'd by several cyber-dojo repos' .travis.yml file.
+//
+// Kudos to...
+// http://kamranicus.com/blog/2015/03/29/triggering-a-travis-build-programmatically/
+// http://kamranicus.com/blog/2015/02/26/continuous-deployment-with-travis-ci/
+
+var Travis = require('travis-ci');
+
+var travis = new Travis({
+  version: '2.0.0',
+  headers: { 'User-Agent': 'Travis/1.0' }
+});
+
+var exit = function(call,error,response) {
+  console.error('ERROR:travis.' + call + 'function(error,response) { ...');
+  console.error('   error:' + error);
+  console.error('response:' + JSON.stringify(response, null, '\t'));
+  process.exit(1);
+};
+
+travis.authenticate({ github_token: process.env.GITHUB_TOKEN }, function(error,response) {
+  var repos = process.argv.slice(2);
+  if (error) { exit('authenticate({...}, ', error, response); }
+  repos.forEach(function(repo) {
+    var parts = repo.split('/');
+    var name = parts[0];
+    var tag = parts[1];
+    console.log('TRIGGERING dependent repo ' + repo);
+    travis.repos(name, tag).builds.get(function(error,response) {
+      if (error) { exit('repos(' + name + ',' + tag + ').builds.get(', error, response); }
+      travis.requests.post({ build_id: response.builds[0].id }, function(error,response) {
+        if (error) { exit('requests.post({...}, ', error, response); }
+        console.log(repo + ':' + response.flash[0].notice);
+      });
+    });
+  });
+});
