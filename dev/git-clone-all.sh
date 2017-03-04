5024d82b67d4e18675c3d14da3d1d69eb89466a0
Jon Jagger <jon@jaggersoft.com>
2017-03-04 21:21:40 +0000

    fix git-clone-all so cloned repos are siblings of home repo

--- dev/git-clone-all.sh
+++ dev/git-clone-all.sh
@@ -4,7 +4,7 @@ set -e
 my_dir="$( cd "$( dirname "${0}" )" && pwd )"
 . ${my_dir}/env-var-repos.sh
 
-cd ../..
+cd ${my_dir}/../..
 for repo in ${CYBER_DOJO_REPOS[@]}; do
   git clone https://github.com/cyber-dojo/${repo}.git
 done
