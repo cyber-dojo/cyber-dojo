ba723da3ba7ca2f8b32cdf713b1bf7f7b88ea364
Jon Jagger <jon@jaggersoft.com>
2017-03-15 20:07:38 +0000

    refactoring; rename shell variables as prometheus/grafana are now live

--- dev/env-var-repos.sh
+++ dev/env-var-repos.sh
@@ -1,5 +1,5 @@
 # these are in a dependency order
 CYBER_DOJO_BASE_REPOS=(ruby sinatra docker web_base nginx)
 CYBER_DOJO_SERVICE_REPOS=(storer zipper differ runner collector commander web)
-CYBER_DOJO_WIP_REPOS=(prometheus grafana)
-CYBER_DOJO_REPOS=("${CYBER_DOJO_BASE_REPOS[@]}" "${CYBER_DOJO_SERVICE_REPOS[@]}" "${CYBER_DOJO_WIP_REPOS[@]}")
+CYBER_DOJO_MONITORING_REPOS=(prometheus grafana)
+CYBER_DOJO_REPOS=("${CYBER_DOJO_BASE_REPOS[@]}" "${CYBER_DOJO_SERVICE_REPOS[@]}" "${CYBER_DOJO_MONITORING_REPOS[@]}")
