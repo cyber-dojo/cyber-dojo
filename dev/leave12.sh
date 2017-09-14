6b1cf326a66b224609d77214eeb750eb5a568048
Jon Jagger <jon@jaggersoft.com>
2017-09-14 15:50:51 +0100

    added scripts for pre-starting 12 avatars for countdown style workshop

---
+++ dev/leave12.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# This is a file assumes all 64 avatars have started
+# in the given kata. It removes 54 named avatars so
+# that only the 12 you want are left.
+# If the kata's ID is E7139819FF then you call this script
+# by passing two arguments E7 139819FF
+# The 12 avatars that it leaves behind are
+#    alligator butterfly dolphin elephant frog
+#    koala lion panda raccoon snake tiger wolf
+# Must be called from _inside_ the storer docker container
+# docker exec -it cyber-dojo-storer sh
+cd /usr/src/cyber-dojo/katas
+cd ${1}
+cd ${2}
+rm -rf antelope bat bear bee beetle buffalo cheetah crab deer eagle
+rm -rf flamingo fox gopher gorilla heron hippo hummingbird hyena
+rm -rf jellyfish kangaroo kingfisher leopard lizard lobster
+rm -rf moose mouse ostrich owl parrot peacock penguin porcupine puffin
+rm -rf rabbit ray rhino salmon seal shark skunk spider squid squirrel
+rm -rf starfish swan toucan tuna turtle vulture walrus whale zebra
---
+++ dev/start64.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Script to start all 64 avatars in a kata
+# with the given ID.
+# enter/start should be a post request but its not.
+# Called from the server that is hosting cyber-dojo.
+
+for run in {1..64}
+do
+  curl \
+    -H "Content-Type: application/json" \
+    --get \
+    http://localhost/enter/start?id=${1}
