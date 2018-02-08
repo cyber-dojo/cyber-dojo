13eacabfe8a9361588b0ebf12f91bd5df9bb253b
Jon Jagger <jon@jaggersoft.com>
2018-02-08 10:43:26 +0000

    fixed build-all.sh script now that all the repos have a common build script name

--- dev/build-all.sh
+++ dev/build-all.sh
@@ -5,8 +5,11 @@ readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 . ${MY_DIR}/env-var-repos.sh
 
 for repo in ${CYBER_DOJO_REPOS[@]}; do
+  echo '------------------------------------------------'
+  echo "BUILDING ${repo}"
+  echo '------------------------------------------------'
   cd ${MY_DIR}/../../${repo} # [1]
-  ./build.sh
+  ./sh/build_docker_images.sh
 done
 
 # [1] docker-compose docs claim you can specify
--- dev/env-var-repos.sh
+++ dev/env-var-repos.sh
@@ -1,4 +1,16 @@
-# these are in a dependency order
-CD_SERVICE_REPOS=(starter storer zipper differ runner_stateful runner_stateless collector commander web nginx)
+# These have a dependency order
+CD_SERVICE_REPOS=(starter \
+                  storer \
+                  zipper \
+                  differ \
+                  runner_stateful \
+                  runner_stateless \
+                  collector \
+                  commander \
+                  web \
+                  nginx)
+
+
 CD_MONITORING_REPOS=(prometheus grafana)
+
 CYBER_DOJO_REPOS=("${CD_SERVICE_REPOS[@]}" "${CD_MONITORING_REPOS[@]}")
