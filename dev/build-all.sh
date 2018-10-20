088c6b5eb93c2218f5011a2da95716d9204c0317
Jon Jagger <jon@jaggersoft.com>
2018-10-20 15:42:04 +0100

    refactoring; delete comments

--- dev/build-all.sh
+++ dev/build-all.sh
@@ -6,15 +6,6 @@ readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 
 for repo in ${CYBER_DOJO_REPOS[@]}; do
   echo '------------------------------------------------'
-  echo "BUILDING ${repo}"
-  echo '------------------------------------------------'
   cd ${MY_DIR}/../../${repo} # [1]
   ./sh/build_docker_images.sh
 done
-
-# [1] docker-compose docs claim you can specify
-# a .env file in the same dir as docker-compose.yml
-# and it will be found. Alas not...
-# https://github.com/docker/compose/issues/3381
-# so I'm explicitly cd'ing.
-
