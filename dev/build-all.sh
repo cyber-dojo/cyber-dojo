1874ddd7d48158221516f665797a7f1b6eb8dd63
Jon Jagger <jon@jaggersoft.com>
2018-12-23 19:32:34 +0000

    fix missing sh/ dir in docker-build

--- dev/build-all.sh
+++ dev/build-all.sh
@@ -7,8 +7,5 @@ readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 for repo in ${CYBER_DOJO_REPOS[@]}; do
   echo '------------------------------------------------'
   cd ${MY_DIR}/../../${repo}
-  pwd
-  ls -al
-  ls -al sh
   ./sh/build_docker_images.sh
 done
