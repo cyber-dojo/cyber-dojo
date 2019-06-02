1a52c53d4ab7eed77ce6b0ab8f8f2379a44932fa
Jon Jagger <jon@jaggersoft.com>
2019-06-02 10:00:35 +0100

    added shared script to trigger dependent repos in service repo ci pipelines

---
+++ dev/trigger-dependent-repos.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -ev
+
+# Service repos curl and run this script in their CI pipelines.
+
+declare -r FROM="${1}"          # eg web-base
+declare -r SHA="${2}"           # eg a9334c964f81800a910dc3d301543262161fbbff
+declare -r REPO_NAMES="${@:3}"  # eg saver ragger ...
+
+declare -r COMMIT_URL="https://github.com/cyber-dojo/${FROM}/commit/${SHA}"
+
+for REPO_NAME in ${REPO_NAMES}; do
+  declare REPO_URL="https://github.com/cyber-dojo/${REPO_NAME}.git"
+  cd /tmp
+  git clone "${REPO_URL}"
+  cd "${REPO_NAME}"
+  declare MESSAGE="automated build trigger from ${COMMIT_URL}"
+  echo "${MESSAGE}" > ${FROM}.trigger
+  git add .
+  git config --global user.email "cyber-dojo-machine-user@cyber-dojo.org"
+  git config --global user.name "Machine User"
+  git commit -m "${MESSAGE}"
+  git push origin master
+done
