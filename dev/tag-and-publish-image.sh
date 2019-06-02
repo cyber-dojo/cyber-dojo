8495b58305cffef104ebce89940d80b5706f0203
Jon Jagger <jon@jaggersoft.com>
2019-06-02 10:04:34 +0100

    add shared script to tag and publish an image; for use in repo's CI pipeline scripts

---
+++ dev/tag-and-publish-image.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+readonly project=${CIRCLE_PROJECT_REPONAME}
+docker tag  cyberdojo/${project}:latest cyberdojo/${project}:${CIRCLE_SHA1:0:7}
+docker tag  cyberdojo/${project}:latest cyberdojo/${project}:beta
+echo "${DOCKER_PASS}" | docker login -u "${DOCKER_USER}" --password-stdin
+docker push cyberdojo/${project}:${CIRCLE_SHA1:0:7}
+docker push cyberdojo/${project}:beta
+docker push cyberdojo/${project}:latest
