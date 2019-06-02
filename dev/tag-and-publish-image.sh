430ab71331d3ccf74ca05c756f853599ed57669c
Jon Jagger <jon@jaggersoft.com>
2019-06-02 10:07:34 +0100

    add comment to script

--- dev/tag-and-publish-image.sh
+++ dev/tag-and-publish-image.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -e
 
+# Service repos curl and run this script in their CI pipelines.
+
 readonly project=${CIRCLE_PROJECT_REPONAME}
 docker tag  cyberdojo/${project}:latest cyberdojo/${project}:${CIRCLE_SHA1:0:7}
 docker tag  cyberdojo/${project}:latest cyberdojo/${project}:beta
+
 echo "${DOCKER_PASS}" | docker login -u "${DOCKER_USER}" --password-stdin
 docker push cyberdojo/${project}:${CIRCLE_SHA1:0:7}
 docker push cyberdojo/${project}:beta
