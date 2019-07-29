10ad1ab028a5c1a7e69b0e783311a954899dd8a1
JonJagger <jon@jaggersoft.com>
2019-07-29 09:49:32 +0100

    add cron and puller to names of services

--- sh/env-var-repos.sh
+++ sh/env-var-repos.sh
@@ -1,21 +1,23 @@
-# Some of these have a dependency order
+# Some of these have a build-order dependency
 
 CYBER_DOJO_REPOS=(rack-base \
                   docker-base \
                   web-base \
+                  starter-base \
                   prometheus \
                   grafana \
-                  starter-base \
+                  cron \
                   custom \
+                  differ \
                   exercises \
                   languages \
-                  zipper \
-                  differ \
-                  saver \
                   mapper \
-                  runner \
-                  ragger \
-                  web \
                   nginx \
+                  puller \
+                  ragger \
+                  runner \
+                  saver \
+                  web \
+                  zipper \
                   versioner \
                   commander)
