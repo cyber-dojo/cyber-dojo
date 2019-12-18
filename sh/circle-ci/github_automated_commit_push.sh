758ad0624175ec8802d3469c282a7262929c6a99
JonJagger <jon@jaggersoft.com>
2019-12-18 06:10:45 +0000

    [ci skip] Add new helper script

---
+++ sh/circle-ci/github_automated_commit_push.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+set -e
+
+# CI pipeline scripts curl and run this script.
+#
+# NB:1 This script does NOT trigger a CircleCI API POST request. See below.
+# NB:2 This script cannot (easily) be run from such a trigger. See below.
+#
+# A CircleCI context must set these two env-vars:
+#   CYBER_DOJO_MACHINE_USER_USERNAME=cyber-dojo-machine-user
+#   CYBER_DOJO_MACHINE_USER_PASSWORD=...Github_Personal_access_Token...
+# And that context must be set in the .circleci/config.yml file
+# using this script.
+
+# The repo whose CI pipeline is running...
+declare -r FROM_ORG="${1}"  # eg cyber-dojo
+declare -r FROM_REPO="${2}" # CIRCLE_PROJECT_REPONAME # eg runner
+declare -r FROM_SHA="${3}"  # CIRCLE_SHA1 # eg a9334c964f81800a910dc3d301543262161fbbff
+
+# The dependent repos, each being triggered via an automated git commit+push
+declare -r TO_ORG="${4}"          # eg cyber-dojo
+declare -r TO_REPO_NAMES="${@:5}" # eg saver ragger ...
+
+declare -r FROM_COMMIT="https://github.com/${FROM_ORG}/${FROM_REPO}/commit/${FROM_SHA}"
+
+for TO_REPO_NAME in ${TO_REPO_NAMES}; do
+  declare TO_REPO_URL="https://github.com/${TO_ORG}/${TO_REPO_NAME}.git"
+  cd /tmp
+  git clone "${TO_REPO_URL}"
+  cd "${TO_REPO_NAME}"
+  declare MESSAGE="Automated build trigger from ${FROM_COMMIT}"
+  echo "${MESSAGE}" > ${FROM_REPO}.trigger
+  git add .
+  git config --global user.email "cyber-dojo-machine-user@cyber-dojo.org"
+  git config --global user.name "CyberDojoMachineUser"
+  git commit -m "${MESSAGE}"
+  declare ORIGIN="https://${CYBER_DOJO_MACHINE_USER_USERNAME}:${CYBER_DOJO_MACHINE_USER_PASSWORD}@github.com/${TO_ORG}/${TO_REPO_NAME}.git"
+  git push "${ORIGIN}" master
+done
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# https://circleci.com/docs/2.0/api-job-trigger/
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# NB:1
+# Triggering a CircleCI workflow via a CircleCI API POST request
+# means the latest git-commit sha (CIRCLE_SHA1) is _NOT_ unique
+# and Helm (Kubernetes) will NOT deploy unless it sees a SHA change.
+#
+# NB:2
+# <quote>
+# Jobs that are triggered via the API do **not** have access
+# to environment variables created for a CircleCI Context
+# </quote>
+# Instead they have to be repeatedly defined for each project.
+# But one project can Import (env) Variables from another project.
+# See https://circleci.com/docs/2.0/env-vars/#setting-an-environment-variable-in-a-project
--- sh/circle-ci/trigger-dependent-repos.sh
+++ sh/circle-ci/trigger-dependent-repos.sh
@@ -2,15 +2,28 @@
 set -ex
 
 # Service repos curl and run this script in their CI pipelines.
+#
+# TODO: Env-var ORG is currently hardwired to cyber-dojo
+#       It will need to be $1 so I can pass in cyber-dojo or cyber-dojo-languages
+#
+# TODO: Automate the git push
+# I think this is done by changing
+#    git push origin master
+# to
+#    USERNAME=cyber-dojo-machine-user
+#    PASSWORD=Personal_API_Token
+#    ORIGIN="https://${USERNAME}:${PASSWORD}@github.com/${ORG}/${REPO_NAME}.git"
+#    git push "${ORIGIN}" master
 
 declare -r FROM="${CIRCLE_PROJECT_REPONAME}" # eg runner
 declare -r SHA="${CIRCLE_SHA1}"              # eg a9334c964f81800a910dc3d301543262161fbbff
 declare -r REPO_NAMES="${@:1}"               # eg saver ragger ...
+declare -r ORG=cyber-dojo
 
-declare -r COMMIT_URL="https://github.com/cyber-dojo/${FROM}/commit/${SHA}"
+declare -r COMMIT_URL="https://github.com/${ORG}/${FROM}/commit/${SHA}"
 
 for REPO_NAME in ${REPO_NAMES}; do
-  declare REPO_URL="https://github.com/cyber-dojo/${REPO_NAME}.git"
+  declare REPO_URL="https://github.com/${ORG}/${REPO_NAME}.git"
   cd /tmp
   git clone "${REPO_URL}"
   cd "${REPO_NAME}"
@@ -18,7 +31,8 @@ for REPO_NAME in ${REPO_NAMES}; do
   echo "${MESSAGE}" > ${FROM}.trigger
   git add .
   git config --global user.email "cyber-dojo-machine-user@cyber-dojo.org"
-  git config --global user.name "Machine User"
+  git config --global user.name "CyberDojoMachineUser"
   git commit -m "${MESSAGE}"
-  git push origin master
+  declare ORIGIN="https://${CYBER_DOJO_MACHINE_USERNAME}:${CYBER_DOJO_MACHINE_PASSWORD}@github.com/${ORG}/${REPO_NAME}.git"
+  git push "${ORIGIN}" master
 done
